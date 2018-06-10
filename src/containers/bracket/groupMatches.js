e5f91dbf6d8e3bf6e3c2c3a3b449aa53b3f88881
Leonardo Dominguez <focuux@gmail.com>
2018-06-10 14:14:04 -0400

    Focus next input when fill the last one

--- src/containers/bracket/groupMatches.js
+++ src/containers/bracket/groupMatches.js
@@ -7,29 +7,52 @@ import matches from '../../matches'
 import { required, number } from './../../services/validations';
 
 class GroupMatches extends React.Component {
+  state = {
+    currentField: 0
+  }
+  constructor(props) {
+    super();
+    this.refs = [];
+  }
   componentDidMount() {
     // console.log(this.props.step);
   }
+
+  onChangeField = e => {
+    // this.refs[1].focus();
+    this.setState((prevState) => ({
+      currentField: prevState.currentField + 1
+    }), () => {
+      if (this.state.currentField < this.refs.length)
+        this.refs[this.state.currentField].focus();
+    });
+  };
+
+  setRef = (element) => {
+    console.log(this.refs, 'REF')
+    this.refs = [...this.refs, element];
+  }
+
+  renderField = ({
+    input,
+    label,
+    type,
+    field,
+    meta: { touched, error, warning }
+  }) => (
+    <div>
+        <label>{touched &&
+          ((error && <span className="inputError">{error}</span>) ||
+            (warning && <span>{warning}</span>))}</label>
+        <input ref={this.setRef} {...input} type={type} className="group__input" maxLength="1" />
+    </div>
+  )
 
 
   render() {
     const { handleSubmit, pristine, reset, submitting, finalStep, prevStep, error, formValues } = this.props
     const currentStep = this.props.step - 1;
-
-    const renderField = ({
-      input,
-      label,
-      type,
-      field,
-      meta: { touched, error, warning }
-    }) => (
-      <div>
-          <label>{touched &&
-            ((error && <span className="inputError">{error}</span>) ||
-              (warning && <span>{warning}</span>))}</label>
-          <input {...input} type={type} className="group__input" maxLength="1" />
-      </div>
-    )
+    const { onChangeField, setRef, renderField } = this;
 
     const button = (this.props.step == finalStep) ? (
       <a className="finishButton" onClick={handleSubmit}>Finish
@@ -63,7 +86,7 @@ class GroupMatches extends React.Component {
                               <p>{team}</p>
                             </div>
                             <div>
-                              <Field name={`${group}_${match}_${team.replace(/\s+/g, '_').toLowerCase()}`} component={renderField} validate={[required, number]} type="text" />
+                              <Field onChange={onChangeField} name={`${group}_${match}_${team.replace(/\s+/g, '_').toLowerCase()}`} component={renderField} validate={[required, number]} type="text" />
                             </div>
                             {!(idx % 2) && <div className="group__divisor">:</div>}
                           </div>
